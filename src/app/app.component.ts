ad8eaeaeb90fcd537c2ec74ecd37549ed5c32abe
vaklarma <biro.andrasgabor.regist@gmail.com>
2019-03-28 17:04:40 +0100

    getData from firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,19 @@ import { AngularFireDatabase } from '@angular/fire/database';
 export class AppComponent  {
   name = 'Angular 6';
 firstnames;
+userData;
   constructor(public app: FirebaseApp,
   private afDb: AngularFireDatabase) {
     this.afDb.list('firstnames')
     .valueChanges()
     .subscribe(
       data => this.firstnames = data
+    );
+
+this.afDb.list('users/qK5jLi6UtqXKqkMiq9agJ0w6u4o1')
+.valueChanges()
+  .subscribe(
+      data => this.userData = data
     );
   }
 }
